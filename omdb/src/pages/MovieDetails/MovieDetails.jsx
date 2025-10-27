2e5174b1d92e5c6c75dfcf279181416a33df85be
Konstantin <crahercenter@gmail.com>
2025-10-27 07:46:35 +0000

    dmitry

--- omdb/src/pages/MovieDetails/MovieDetails.jsx
+++ omdb/src/pages/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react"
+import { useParams } from "react-router"
+
 const MovieDetails = () => {
+    const { id } = useParams()
+
+    useEffect(() => {
+        const hadleLoad = async () => {
+            try {
+                const res = fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_APIKEY}&i=${id}`)
+
+            } catch (error) {
+                console.error(error)
+            }
+
+        }
+        hadleLoad()
+    }, [id])
     return (
         <>
             <h1>Фильм</h1>
